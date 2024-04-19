73150938ae2e84e0b4d65b139427c80ff8ea8dcb
jbeckwith <jsbeckwith@gmail.com>
2024-04-19 11:30:45 +0100

    updated requirements

--- docs/conf.py
+++ docs/conf.py
@@ -9,12 +9,12 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.abspath('../src')))
-sys.path.insert(0, os.path.abspath(os.path.abspath('..')))
+#sys.path.insert(0, os.path.abspath(os.path.abspath('..')))
 
 project = 'pyRASP'
 copyright = '2024, Joseph S. Beckwith, Bin Fu, Steven F. Lee'
 author = 'Joseph S. Beckwith, Bin Fu'
-release = 'v0.5.0'
+release = 'v0.6.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -41,4 +41,4 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+#html_static_path = ['_static']
