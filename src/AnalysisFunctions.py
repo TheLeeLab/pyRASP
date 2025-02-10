14a772e31c91ee5cd132dfaaae2f445fa3e6bae3
JBeckwith <jsbeckwith@gmail.com>
2025-02-10 21:22:39 +0000

    Updated

--- src/AnalysisFunctions.py
+++ src/AnalysisFunctions.py
@@ -134,9 +134,18 @@ class Analysis_Functions:
         indices = None
         for i in np.arange(len(image_size)):
             if indices is None:
-                indices = np.where((0 >= coords[:, i]) or (coords[:, i] < image_size[i]))[0]
+                indices = np.where(
+                    (0 >= coords[:, i]) or (coords[:, i] < image_size[i])
+                )[0]
             else:
-                indices = np.hstack([indices, np.where((0 >= coords[:, i]) or (coords[:, i] < image_size[i]))[0]])
+                indices = np.hstack(
+                    [
+                        indices,
+                        np.where((0 >= coords[:, i]) or (coords[:, i] < image_size[i]))[
+                            0
+                        ],
+                    ]
+                )
         coords = coords[indices, :]
         return np.ravel_multi_index(coords.T, image_size, order="F")
 
